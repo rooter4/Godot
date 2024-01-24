d05d86c591ce6d7d3ce5d9cf6900c1a5cfccde28
laptop <luke.root4@gmail.com>
2024-01-24 20:26:41 +0900

    animationplayer

--- Scenes/main_character.gd
+++ Scenes/main_character.gd
@@ -6,14 +6,15 @@ const JUMP_VELOCITY = -900.0
 @onready var sprite_2d = $Sprite2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var slam_mation = $AnimationPlayer
 
 func _physics_process(delta):
+	
 	
 	if (velocity.x >1 || velocity.x < -1):
 		sprite_2d.animation ="running"
 	elif (Input.is_action_pressed("ui_down") and is_on_floor()):
-			sprite_2d.animation = "pound"
+			slam_mation.play("slam")
 	else: 
 		sprite_2d.animation = "idle"
 	
